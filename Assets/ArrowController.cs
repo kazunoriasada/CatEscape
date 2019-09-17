4b977ed17e19cdaeae5aa1cec20e2fbc0f270207
浅田和則 <kazu.july05@gmail.com>
2019-09-17 19:10:34 +0900

    UISetting

---
+++ Assets/ArrowController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -0.1f, 0);
+
+        if(transform.position.y < -5.0f)
+        {
+            Destroy(gameObject);
+        }
+        //当たり判定
+        Vector2 p1 = transform.position; //矢の中心座標
+        Vector2 p2 = this.player.transform.position; //プレイヤーの中心座標
+        Vector2 dir = p1 - p2;
+
+        float d = dir.magnitude;
+        float r1 = 0.5f;
+        float r2 = 0.8f;
+
+        if (d < r1 + r2)
+        {
+            //監督スクリプトにプレイヤーが衝突したことを伝える
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().DecreaseHp();
+            
+            //衝突した矢を消す
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject hpGauge;
+
+    void Start()
+    {
+        //Find = シーン中の中から該当するオブジェクトを探し、変数に代入する
+        this.hpGauge = GameObject.Find("hpGauge");
+    }
+
+    public void DecreaseHp()
+    {
+        this.hpGauge.GetComponent<Image> ().fillAmount -= 0.1f;
+    }
+}
+    
